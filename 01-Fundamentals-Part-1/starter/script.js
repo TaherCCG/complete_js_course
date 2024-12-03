8930ae891c8820c4b09d27bab40a7060cc0d09fc
TaherCCG <taher.ccg@gmail.com>
2024-12-03 12:52:19 +0000

    Add initial HTML and JavaScript files for JavaScript Fundamentals – Part 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,39 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!');
+
+console.log(40 + 8 + 23 - 10);
+
+// Values and Variables
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+
+// Assignment 1
+let country = 'United Kingdom';
+let continent = 'Europe';
+let population = 68;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
