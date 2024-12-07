bb16221eb61c38d6a7fce4364117eae9b3502805
TaherCCG <taher.ccg@gmail.com>
2024-12-07 21:06:46 +0000

    Add short-circuiting.js: Implement examples and explanations for logical operators && and ||

--- 09-Data-Structures-Operators/short-circuiting.js
+++ 09-Data-Structures-Operators/short-circuiting.js
@@ -50,3 +50,40 @@ const restaurant = {
     );
   },
 };
+
+// Short Circuiting (&& and ||)
+// Short Circuiting is a way to use the logical operators to do something that is similar to an if statement.
+// The logical operators are used to short-circuit the evaluation of the expression, which means that the evaluation stops as soon as the result is known.
+
+console.log('---- OR ----');
+// the OR operator will return the first truthy value of the two values. If both values are falsy, it will return the last value.
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+// the AND operator will return the first falsy value of the two values. If both values are truthy, it will return the last value.
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+// The above if statement can be replaced with the following:
+// this will short-circuit if the restaurant.orderPizza is not defined.  If it is defined, it will call the function. This is a cleaner way to write the if statement.
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
